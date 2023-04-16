d37d60c4c1697344e4f794a4b6f7e989cba77371
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-04-16 14:13:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+let timer = document.getElementById("timer");
+
+function updateTime(params) {
+	const date = new Date();
+	timer.innerHTML = date;
+}
+setInterval(updateTime , 1000);
